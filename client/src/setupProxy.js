f3f9d48429611139258a46e09fe0c2e2bc7b4a7f
James Vinter <thevinternet@users.noreply.github.com>
2021-06-29 16:02:24 +0100

    Core Client App Config Updates
    - Dockerfile Updated
    - ENV example File Updated
    - Package Dependencies
    - Routing Setup Updated
    - Public Assets Updated

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -9,5 +9,12 @@ module.exports = function(app) {
 			target: `${process.env.REACT_APP_SERVER_PROXY_HOST}:${process.env.REACT_APP_SERVER_PROXY_PORT}`,
       changeOrigin: false
     })
+	);
+	app.use(
+    '/mongoexpress',
+    createProxyMiddleware({
+			target: `${process.env.REACT_APP_MONGO_EXPRESS_PROXY_HOST}:${process.env.REACT_APP_MONGO_EXPRESS_PROXY_PORT}`,
+      changeOrigin: false
+    })
   );
 };
